2c940cb67e7052c6c46449e2ca29f58fcbd03818
Neme Velazquez <nav7977@rit.edu>
2023-09-20 18:30:29 -0400

    PE8 - Question 7

---
+++ PE8-Q5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8_Q5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,,] values = new double[21,21,21];
+            for (double d = -1; d <= 1; d += 0.1)
+            {
+                z
+            }
+        }
+    }
+}
---
+++ PE8-Q7/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8_Q7
+{
+    /* Author: Neme Velazquez
+     * Purpose: Create a program that accepts a string and returns it in reverse order
+     * Restrictions: None
+     */
+    static internal class Program
+    {
+        /* Purpose: Ask user to enter a string, convert it into characters, and print it in reverse order
+         * Restrictions: None
+         * Returns: reversed string of original
+         */
+        static void Main(string[] args)
+        {
+            //variable - stores user input
+            string inputString = "";
+            
+            //prompt user for input and store
+            Console.Write("Hello! Enter some words, some characters, some keyboard smash if you will: ");
+            inputString = Console.ReadLine();
+
+            //convert string to array of characters and creates empty array to be added to (reverse string)
+            char[] userString = inputString.ToCharArray();
+            char[] outputString = new char[userString.Length];
+
+            
+            //add characters of original string in reverse order of which it was added into new empty string
+            for (int i = 0; i < userString.Length; i++)
+            {
+                outputString[i] = userString[userString.Length - (i + 1)];
+            }
+
+            //print so user can see reversed string
+            Console.WriteLine(); //added for readibility/separation between tasks
+            Console.WriteLine("Ta-da!");
+            Console.WriteLine(outputString);
+        }
+    }
+}
