a3dddecc123e0735aa1361c1feac97010fa6c9e2
Neme Velazquez <nav7977@rit.edu>
2023-09-24 19:23:48 -0400

    PE9 - Question 3 (Question 2 mislabbelled, this is correct)

---
+++ Real PE9 - Q3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Real_PE9___Q3
+{
+    delegate string NewReadLine(); 
+    /* Author: Neme Velazquez
+     * Purpose: Use a delegate program to copy readline
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NewReadLine processReadConsole;
+
+            processReadConsole = Console.ReadLine;
+
+            string sInput = processReadConsole();
+        }  
+    }
+}
