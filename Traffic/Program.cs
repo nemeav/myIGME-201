27af119955e8206e5ec173f18ee48965a1638e31
Neme Velazquez <nav7977@rit.edu>
2023-10-10 20:12:06 -0400

    PE11-Question 6

--- Traffic/Program.cs
+++ Traffic/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vehicles;
 
 namespace Traffic
 {
@@ -10,6 +11,17 @@ namespace Traffic
     {
         static void Main(string[] args)
         {
+            /* trying to pass non-IPassengerCarrier object into function
+             * gets compile time error :( - makes sense though
+             */
+            /*FreightTrain freightTrain = new FreightTrain();
+            AddPassenger(freightTrain);*/
+        }
+
+        public void AddPassenger(IPassengerCarrier carrier)
+        {
+            carrier.LoadPassenger();
+            Console.WriteLine(carrier.ToString());
         }
     }
 }
