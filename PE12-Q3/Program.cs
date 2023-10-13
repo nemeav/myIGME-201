607cc7fb24af9ed7091b93b996fc226aa625cfb4
Neme Velazquez <nav7977@rit.edu>
2023-10-13 15:45:04 -0400

    PE12 - Question 3

---
+++ PE12-Q3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE12_Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+    }
+
+    public class MyClass
+    {
+        private string myString;
+        public string MyString
+        {
+            set { myString = "hello"; }
+        }
+
+        public virtual string GetString()
+        {
+            return myString;
+        }
+    }
+
+    public class MyDerivedClass : MyClass
+    {
+        public override string GetString()
+        {
+            string newString = base.GetString();
+            newString += " (output from the derived class)";
+            return newString;
+        }
+    }
+}
