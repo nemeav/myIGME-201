a0323a5667ce05ca4eab1ddce81e5f56cbaacc22
Neme Velazquez <nav7977@rit.edu>
2023-10-19 17:50:37 -0400

    PE14 - Question 3

---
+++ PE14-Q3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE14_Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //new objects
+            FirstClass firstClass = new FirstClass();
+            SecondClass secondClass = new SecondClass();
+
+            //method calls
+            MyMethod(firstClass);
+            MyMethod(secondClass);
+        }
+
+        //separate method references the object methods
+        public static void MyMethod(object myObject)
+        {
+            IMyInterface myInterfaceObject = (IMyInterface)myObject;
+            myInterfaceObject.ObjMethod();
+        }
+    }
+
+    //interface to be inherited from
+    public interface IMyInterface
+    {
+        void ObjMethod();
+    }
+
+    public class FirstClass : IMyInterface
+    {
+        public void ObjMethod()
+        {
+            Console.WriteLine("This is from the first class!");
+        }
+    }
+
+    public class SecondClass : IMyInterface
+    {
+        public void ObjMethod()
+        {
+            Console.WriteLine("This is from the second class!");
+        }
+    }
+}
